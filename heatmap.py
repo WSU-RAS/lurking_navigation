52fdae55c2771eb51641be9d2adcd9ea21553274
Yulia <julia.maliauka953@myci.csuci.edu>
2018-06-07 15:29:14 -0700

    Added dictionary for kyoto's sensor names and coordinates in meters.

--- heatmap.py
+++ heatmap.py
@@ -1,4 +1,3 @@
-
 # language imports
 import sys
 
@@ -6,20 +5,92 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 
-# custom imports
-import yulias_thing
-
-
 def get_sensor_map():
     """ get sensor map
 
-    builds and returns a hardcoded dictionary mapping sensor names to their locations in Kyoto
+    builds and returns a hardcoded dictionary mapping sensor names to their locations in the kyoto smarthome
 
     key: string, sensor name
     value: tuple, (x, y)
 
     """
-    return {}
+    sensor_map = {}
+    sensor_map["D002"] = (3.625,0.1125)
+    sensor_map["T102"] = (3.625,0.1125)
+    sensor_map["M004"] = (0.875,0.6875)
+    sensor_map["M005"] = (2.0625,0.6875)
+    sensor_map["M011"] = (3.3125,0.6875)
+    sensor_map["M012"] = (4.525,0.6875)
+    sensor_map["M003"] = (0.875,1.9375)
+    sensor_map["M006"] = (2.0625,1.9375)
+    sensor_map["M010"] = (3.3125,1.9375)
+    sensor_map["M013"] = (4.525,1.9375)
+    sensor_map["T001"] = (2.0625,1.9375)
+    sensor_map["M002"] = (0.875,3.125)
+    sensor_map["M007"] = (2.0625,3.125)
+    sensor_map["M009"] = (3.3125,3.125)
+    sensor_map["M014"] = (4.525,3.125)
+    sensor_map["D013"] = (0.875,3.5)
+    sensor_map["M008"] = (2.75,3.5)
+    sensor_map["M001"] = (1.5625,4.0)
+    sensor_map["M015"] = (4.1875,4.0)
+    sensor_map["M023"] = (1.5625,5.15)
+    sensor_map["M053"] = (4.1875,5.15)
+    sensor_map["M016"] = (4.1875,5.15)
+    sensor_map["M022"] = (1.5625,5.375)
+    sensor_map["M017"] = (4.125,5.65)
+    sensor_map["T002"] = (4.125,5.65)
+    sensor_map["M021"] = (1.5625,6.5375)
+    sensor_map["M019"] = (2.8125,6.5375)
+    sensor_map["M018"] = (4.125,6.5375)
+    sensor_map["M026"] = (0.6,6.625)
+    sensor_map["M020"] = (2.8125,7.275)
+    sensor_map["M051"] = (4.0625,7.8125)
+    sensor_map["M025"] = (0.6,7.6875)
+    sensor_map["M024"] = (1.5,7.5625)
+    sensor_map["T101"] = (0.9375,8.40)
+    sensor_map["D001"] = (0.9375,8.50)
+
+    sensor_map["M041"] = (-1,-1)
+    sensor_map["M032"] = (-1,-1)
+    sensor_map["M039"] = (-1,-1)
+    sensor_map["M027"] = (-1,-1)
+    sensor_map["M045"] = (-1,-1)
+    sensor_map["M037"] = (-1,-1)
+    sensor_map["M028"] = (-1,-1) 
+    sensor_map["M030"] = (-1,-1)
+    sensor_map["M057"] = (-1,-1)
+    sensor_map["M029"] = (-1,-1)
+    sensor_map["M031"] = (-1,-1)
+    sensor_map["M036"] = (-1,-1)
+    sensor_map["M033"] = (-1,-1)
+    sensor_map["M034"] = (-1,-1)
+    sensor_map["M038"] = (-1,-1)
+    sensor_map["M040"] = (-1,-1)
+    sensor_map["M055"] = (-1,-1) 
+    sensor_map["M054"] = (-1,-1)
+    sensor_map["M056"] = (-1,-1)
+    sensor_map["M042"] = (-1,-1)
+    sensor_map["M043"] = (-1,-1)
+    sensor_map["M035"] = (-1,-1)
+    sensor_map["M044"] = (-1,-1)
+    sensor_map["M050"] = (-1,-1)
+    sensor_map["M049"] = (-1,-1)
+    sensor_map["M046"] = (-1,-1)
+    sensor_map["M048"] = (-1,-1)
+    sensor_map["M047"] = (-1,-1)
+    sensor_map["M052"] = (-1,-1)
+    sensor_map["M058"] = (-1,-1)
+    sensor_map["M059"] = (-1,-1)
+    sensor_map["M060"] = (-1,-1)
+    sensor_map["MA201"] = (-1,-1)
+    sensor_map["MA202"] = (-1,-1)
+    sensor_map["MA203"] = (-1,-1)
+    sensor_map["MA204"] = (-1,-1)
+    sensor_map["MA205"] = (-1,-1)
+    sensor_map["MA206"] = (-1,-1)
+    sensor_map["MA207"] = (-1,-1)
+    return sensor_map
 
 class Heatmap():
     """ Heatmap class for storing human activity hotness
