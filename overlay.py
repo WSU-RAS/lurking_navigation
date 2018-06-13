d88533b47f3af724dbe849773d593c97e1065185
daymare <daylankelting@yahoo.com>
2018-06-13 13:19:36 -0700

    added path map to overlay

--- overlay.py
+++ overlay.py
@@ -8,20 +8,33 @@ from matplotlib.colors import LinearSegmentedColormap
 
 from slam_map import SlamMap
 from heatmap import Heatmap
+from path_map import PathMap
 
 def main():
     # get command line arguments        
     slam_data_filepath = sys.argv[1]
     smarthome_data_filepath = sys.argv[2]
 
+    # get slam map
     slam_map = SlamMap(slam_data_filepath)
+
+    # get heatmap
     heatmap = Heatmap(smarthome_data_filepath)
     heatmap.set_offset(slam_map.origin[0], slam_map.origin[1])
 
-    # plot the slam map
+    # get path map
+    path_map = PathMap(heatmap)
+
+    # plot the path map
+    path_array = path_map.get_as_array()
+    plt.imshow(np.transpose(path_array), cmap='hot', interpolation='nearest')
+
+    # plot the heatmap
     heatmap = heatmap.get_heatmap_array()
-    plt.imshow(np.transpose(heatmap), cmap='hot', interpolation='nearest')
-    plt.imshow(np.transpose(slam_map.map), cmap=get_custom_colormap(), interpolation='nearest')
+    plt.imshow(np.transpose(heatmap), cmap=get_custom_colormap_blue(), interpolation='nearest')
+
+    # plot the slam map
+    plt.imshow(np.transpose(slam_map.map), cmap=get_custom_colormap_green(), interpolation='nearest')
 
     # flip y axis
     axis = plt.gca()
@@ -30,7 +43,7 @@ def main():
     plt.show()
 
 
-def get_custom_colormap():
+def get_custom_colormap_green():
     cdict = \
             {
                 'red': ((0.0, 0.0, 0.0),
@@ -39,6 +52,21 @@ def get_custom_colormap():
                         (1.0, 1.0, 1.0)),
                 'blue': ((0.0, 0.0, 0.0),
                         (1.0, 0.5, 0.5)),
+                'alpha': ((0.0, 0.0, 0.0),
+                          (0.1, 1.0, 1.0),
+                          (1.0, 1.0, 1.0))
+            }
+    return LinearSegmentedColormap('alpha_red', cdict)
+
+def get_custom_colormap_blue():
+    cdict = \
+            {
+                'red': ((0.0, 0.0, 0.0),
+                        (1.0, 0.0, 0.0)),
+                'green': ((0.0, 0.0, 0.0),
+                          (1.0, 0.0, 0.0)),
+                'blue': ((0.0, 0.0, 0.0),
+                         (1.0, 1.0, 1.0)),
                 'alpha': ((0.0, 0.0, 0.0),
                           (0.1, 1.0, 1.0),
                           (1.0, 1.0, 1.0))
