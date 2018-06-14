986baf73b15bf9a77d74b37ac48e9c6455b42990
Julia Maliauka <julia.maliauka953@myci.csuci.edu>
2018-06-14 16:31:23 -0700

    Added external data file support for heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -25,15 +25,26 @@ def get_sensor_map(filepath):
 
     """
     sensor_map = {}
-    ignored_sensors = {}
+    ignored_sensors = []
 
     data_file = open(filepath, "r")
 
+    with data_file as file_pointer:
+        line = "empty"
 
+        while line:
+            line = file_pointer.readline()
+            line = line.replace('\r\n', '')
+            sensor_information = line.split()
 
-    # sensor_map["D002"] = (0.1125, 3.625)
+            # Only add the sensor to the map if we know where it is on the map 
+            if (len(sensor_information) > 1):
+                sensor_map[sensor_information[0]] = (float(sensor_information[1]), float(sensor_information[2]))
+            elif (len(sensor_information) == 1): 
+                # Add to the list of ignored sensors since it doesn't have coordinates 
+                ignored_sensors.append(sensor_information[0])
     
-
+    data_file.close() 
     return sensor_map, ignored_sensors
 
 class Heatmap():
@@ -45,8 +56,9 @@ class Heatmap():
 
     No public attributes
 
+
     """
-    def __init__(self, smarthome_data_filepath):
+    def __init__(self, sensor_list_filepath, smarthome_data_filepath):
         # map of points, key: (x, y) value: heat value
         self.point_map = {}
 
@@ -56,39 +68,11 @@ class Heatmap():
         self.map_resolution = 0.125
         self.x_offset = 0 # x offset in map points
         self.y_offset = 0 # y offset in map points
-
-        # self.load_from_file(smarthome_data_filepath)
-
-        self.sensor_map = {}
-        self.ignored_sensors = {}
-
-    def load_from_file(self, filepath):
-        """ load heatmap from filepath
-
-        get all the data out from a file and load into our heatmap
-        then normalize our heatmap.
-
-        """
         # load the sensor map and a list of ignore sensors 
-        self.sensor_map, self.ignored_sensors = get_sensor_map(filepath)
 
+        self.sensor_map, self.ignored_sensors = get_sensor_map(sensor_list_filepath)
         print self.sensor_map
         print self.ignored_sensors
-
-        # build the heatmap
-        next_point = self._get_next_point(data_file, self.sensor_map, self.ignored_sensors)
-        while next_point is not None:
-            # add to the sensor map
-            if next_point in self.point_map:
-                self.point_map[next_point] += 1
-            else:
-                self.point_map[next_point] = 1
-
-            # get the next point
-            next_point = self._get_next_point(data_file, self.sensor_map, self.ignored_sensors)
-
-        # normalize the heatmap
-        self._normalize_heatmap()
 
     def set_offset(self, offset_x, offset_y):
         """ Set the origin offset of the heatmap
@@ -110,7 +94,7 @@ class Heatmap():
                 due to floating point precision
         
         """
-        # setup variables
+        # set up variables
         old_x_offset = self.x_offset
         old_y_offset = self.y_offset
         new_x_offset = offset_x
@@ -174,39 +158,6 @@ class Heatmap():
 
         axis.set_ylim(axis.get_ylim()[::-1])
         plt.show()
-
-    def _get_next_point(self, file, sensor_map, ignored_sensors):
-        """ get the next point out of 
-        """
-        while True:
-            next_line = file.readline()
-
-            # check if the file is empty
-            if len(next_line) <= 0:
-                return None
-
-            data = DataLine(next_line)
-
-            if data.sensor_type == "Control4-Motion" and data.message == "ON" and data.sensor_name not in ignored_sensors:
-                # found a motion sensor trigger on the 1st floor
-                point = sensor_map[data.sensor_name]
-                return point
-
-    def _normalize_heatmap(self):
-        """ normalize the heatmap
-
-        divide each value of the point_map by the sum of all values.
-
-        """
-        # sum up all the values
-        sum = 0
-        for key, value in self.point_map.iteritems():
-            sum += value
-
-        # divide each entry by the sum
-        for key, value in self.point_map.iteritems():
-            new_value = value / float(sum)
-            self.point_map[key] = new_value
 
 class DataLine():
     """ Data line
--- static_heatmap.py
+++ static_heatmap.py
@@ -2,15 +2,75 @@
     Static Heatmap
 """
 import sys
-
 from heatmap import Heatmap 
 
 class StaticHeatmap(Heatmap):
-    def __init__(self, smarthome_data_filepath):
-        Heatmap.__init__(self, smarthome_data_filepath)
+    def __init__(self, sensor_list_filepath, smarthome_data_filepath):
+        Heatmap.__init__(self, sensor_list_filepath, smarthome_data_filepath)
+
+    def _load_from_file(self, filepath):
+        """ load heatmap from filepath
+
+        get all the data out from a file and load into our heatmap
+        then normalize our heatmap.
+
+        """
+        
+
+        print self.sensor_map
+        print self.ignored_sensors
+
+        # build the heatmap
+        next_point = self._get_next_point(data_file, self.sensor_map, self.ignored_sensors)
+        while next_point is not None:
+            # add to the sensor map
+            if next_point in self.point_map:
+                self.point_map[next_point] += 1
+            else:
+                self.point_map[next_point] = 1
+
+            # get the next point
+            next_point = self._get_next_point(data_file, self.sensor_map, self.ignored_sensors)
+
+        # normalize the heatmap
+        self._normalize_heatmap()
+
+    def _get_next_point(self, file, sensor_map, ignored_sensors):
+        """ get the next point out of 
+        """
+        while True:
+            next_line = file.readline()
+
+            # check if the file is empty
+            if len(next_line) <= 0:
+                return None
+
+            data = DataLine(next_line)
+
+            if data.sensor_type == "Control4-Motion" and data.message == "ON" and data.sensor_name not in ignored_sensors:
+                # found a motion sensor trigger on the 1st floor
+                point = sensor_map[data.sensor_name]
+                return point
+
+    def _normalize_heatmap(self):
+        """ normalize the heatmap
+
+        divide each value of the point_map by the sum of all values.
+
+        """
+        # sum up all the values
+        sum = 0
+        for key, value in self.point_map.iteritems():
+            sum += value
+
+        # divide each entry by the sum
+        for key, value in self.point_map.iteritems():
+            new_value = value / float(sum)
+            self.point_map[key] = new_value
 
 if __name__ == "__main__":
     # Take in the file we will construct a heatmap from 
-    smarthome_data_filepath = sys.argv[1]
+    sensor_list_filepath = sys.argv[1]
+    smarthome_data_filepath = sys.argv[2]
 
-    static_heatmap = StaticHeatmap(smarthome_data_filepath) 
+    static_heatmap = StaticHeatmap(sensor_list_filepath, smarthome_data_filepath) 
