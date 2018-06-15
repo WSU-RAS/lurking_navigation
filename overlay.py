88b7d07a37f1dc086118142bfcffe3de2f84d2b2
daymare <daylankelting@yahoo.com>
2018-06-15 15:26:40 -0700

    updated overlay to work with the new heatmap architecture

--- overlay.py
+++ overlay.py
@@ -7,22 +7,23 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
 from slam_map import SlamMap
-from heatmap import Heatmap
+from static_heatmap import StaticHeatmap
 from path_map import PathMap
 from weighted_average import WeightedAverage
 from reachability_map import ReachabilityMap
 
 def main():
-    # get command line arguments        
+    # get command line arguments
     slam_data_filepath = sys.argv[1]
     smarthome_data_filepath = sys.argv[2]
+    sensor_map_filepath = sys.argv[3]
 
     ### get all the things to plot
     # get slam map
     slam_map = SlamMap(slam_data_filepath)
 
     # get heatmap
-    heatmap = Heatmap(smarthome_data_filepath)
+    heatmap = StaticHeatmap(sensor_map_filepath, smarthome_data_filepath)
     heatmap.set_offset(slam_map.origin[0], slam_map.origin[1])
 
     # get path map
@@ -41,17 +42,17 @@ def main():
 
     # plot the heatmap
     heatmap = heatmap.get_heatmap_array()
-    #plt.imshow(np.transpose(heatmap), cmap=get_custom_colormap_blue(), interpolation='nearest')
+    plt.imshow(np.transpose(heatmap), cmap=get_custom_colormap_blue(), interpolation='nearest')
 
     # plot the slam map
-    plt.imshow(np.transpose(slam_map.map), cmap=get_custom_colormap_green(), interpolation='nearest')
+    #plt.imshow(np.transpose(slam_map.map), cmap=get_custom_colormap_green(), interpolation='nearest')
 
     # plot the weighted average
     average_point = weighted_average.get_weighted_average_point()
-    #plt.plot(average_point[0], average_point[1], 'go')
+    plt.plot(average_point[0], average_point[1], 'go')
 
     # plot the reachability map
-    plt.imshow(np.transpose(reachability_map.map), cmap=get_custom_colormap_blue(), interpolation='nearest')
+    #plt.imshow(np.transpose(reachability_map.map), cmap=get_custom_colormap_blue(), interpolation='nearest')
 
     # flip y axis
     axis = plt.gca()
