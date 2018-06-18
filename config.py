53c4a52f2168a7a5e982b6fcabfb5457da8deb74
daymare <daylankelting@yahoo.com>
2018-06-18 14:54:33 -0700

    debugged config

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ class Config():
     """
     def __init__(self, config_filepath=""):
         # variables
-        self.map_width = 0.0
-        self.map_height = 0.0
-        self.map_resolution = 0.0
-        self.slam_origin = (0, 0)
-        self.slam_rotation = 0
-        self.slam_uncertainty_cutoff = 0
+        self.map_width = 0.0 # in meters
+        self.map_height = 0.0 # in meters
+        self.map_resolution = 0.0 # in meters
+        self.slam_origin = (0, 0) # map indecies
+        self.slam_rotation = 0 # in degrees
+        self.slam_uncertainty_cutoff = 0 # in slam map values
 
         if config_filepath != "":
             self.load_from_file(config_filepath)
@@ -30,13 +30,14 @@ class Config():
             line = config_file.readline()
 
             # check for lines of only whitespace
-            if line.isspace():
+            if len(line.strip()) == 0:
                 continue
 
             # process this line
             line_list = line.split("=")
             key = line_list[0].strip()
-            value = line_list[1].strip()
+            value = eval(line_list[1].strip())
+            print type(value), value
 
             config_dict[key] = value
 
@@ -47,5 +48,3 @@ class Config():
         self.slam_origin = config_dict["slam_origin"]
         self.slam_rotation = config_dict["slam_rotation"]
         self.slam_uncertainty_cutoff = config_dict["slam_uncertainty_cutoff"]
-
-
