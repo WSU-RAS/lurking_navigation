29b66451000faff2ac2014cc4374b8942ff03227
Julia Maliauka <julia.maliauka953@myci.csuci.edu>
2018-06-18 13:08:49 -0700

    Added initialization and decay methods to dynamic_heatmap.py. Needs to be incorporated into realtime data.

--- dynamic_heatmap.py
+++ dynamic_heatmap.py
@@ -1,8 +1,13 @@
 import sys
+import math
+
 from heatmap import Heatmap 
 
+TIME_TICK_LENGTH = 8           # in seconds
+HEATMAP_DECAY_STRENGTH = 0.8   # how quickly the heatmap decays
+
 class DynamicHeatmap(Heatmap):
-    def __init__(self, sensor_list_filepath, smarthome_data_source):
+    def __init__(self, sensor_list_filepath):
         Heatmap.__init__(self, sensor_list_filepath)
 
         """
@@ -10,17 +15,44 @@ class DynamicHeatmap(Heatmap):
             update heatmap every time tick 
             display every time tick 
         """
+        self.heatmap = [
+                    [0.0 for j in range(int(math.ceil(self.map_height / self.map_resolution)))] 
+                    for i in range(int(math.ceil(self.map_width / self.map_resolution)))
+                  ]
 
-        print smarthome_data_source
-
+    def _init_ras_location(self):
+        print "nothing yet"
+        return (42, 42)
+    
     def _import_sensor_information(self):
         print "wow what a great method"
 
-    def 
+    def get_heatmap_array(self):
+        """ get heatmap array
+
+        get the heatmap as a 2d array indicating the hotness at each area.
+        Note that the heatmap will be either normalized or not based on whether _normalize_heatmap has been run.
+
+        """
+        self.heatmap = self._decay_heatmap(self.heatmap)
+
+        return self.heatmap
+
+    def _decay_heatmap(self, heatmap):
+        """
+            Decay every point in the heatmap by a certain preset percentage. 
+        """
+        
+        for row_index, row in enumerate(heatmap):
+            for col_index, point in enumerate(row): 
+                heatmap[row_index][col_index] += 3
+        
+        return heatmap
 
 if __name__ == "__main__":
     # Acquire input source for realtime heatmap generation 
     sensor_list_filepath = sys.argv[1]
-    smarthome_data_source = sys.argv[2]
 
-    dynamic_heatmap = DynamicHeatmap(sensor_list_filepath, smarthome_data_source) 
+    dynamic_heatmap = DynamicHeatmap(sensor_list_filepath) 
+
+    dynamic_heatmap.get_heatmap_array()
--- static_heatmap.py
+++ static_heatmap.py
@@ -26,7 +26,7 @@ class StaticHeatmap(Heatmap):
             # add to the sensor map
             if next_point in self.point_map:
                 self.point_map[next_point] += 1
-            else:
+            else: 
                 self.point_map[next_point] = 1
 
             # get the next point
@@ -36,8 +36,6 @@ class StaticHeatmap(Heatmap):
         self._normalize_heatmap()
 
     def _get_next_point(self, smarthome_data_file, sensor_map, ignored_sensors):
-        """ get the next point out of 
-        """
         while True:
             next_line = smarthome_data_file.readline()
 
