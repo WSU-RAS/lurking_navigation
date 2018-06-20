f5470b65eff13ccdd7966a4ab789b950b8aace84
git-yulia <julia.maliauka@myci.csuci.edu>
2018-06-20 10:42:14 -0700

    Modified display_heatmap to not flip y-axis every time.

--- dynamic_heatmap.py
+++ dynamic_heatmap.py
@@ -12,13 +12,15 @@ from config import Config
 from heatmap import Heatmap
 from ras_msgs.msg import SensorPub
 
-TIME_TICK_LENGTH = 1           # in seconds
+TIME_TICK = 1                  # in seconds
 HEATMAP_DECAY_STRENGTH = 0.8   # how quickly the heatmap decays
 
 class DynamicHeatmap(Heatmap):
     def __init__(self, sensor_list_filepath, config):
         Heatmap.__init__(self, sensor_list_filepath, config)
         self.heatmap = self.get_heatmap_array()
+
+
 
     @staticmethod
     def update(triggered_sensor):
@@ -46,44 +48,28 @@ class DynamicHeatmap(Heatmap):
         heatmap[rand][rand] += 10
         return heatmap
 
+    """
+        Displays heatmap once - called every time tick from lurking_ai 
+    """
     def display_heatmap(self):
-        program_status = True 
 
-        # Initialize the heatmap plot 
         # heatmap_array = self.get_heatmap_array()
-        np_heatmap = np.array(self.heatmap)
-        axis = plt.gca()
-        plt.imshow(np.transpose(np_heatmap), cmap='hot', interpolation='nearest')
-        axis.set_ylim(axis.get_ylim()[::-1])
-        #plt.show()
+        self.heatmap = self.increment_heatmap_array(self.heatmap)
 
+        np_heatmap = np.array(self.heatmap)
+        plt.imshow(np.transpose(np_heatmap), cmap='hot', interpolation='nearest')
+
+        axis = plt.gca()
+        
+        if (axis.get_ylim()[0] > axis.get_ylim()[1]):
+            axis.set_ylim(axis.get_ylim()[::-1])
+
+        # print axis.get_ylim()[0] 
+
+        # makes sure our origin is correctly set without flipping the y-axis
+        # axis.set_ylim(axis.get_ylim()[0])
+
+        # plt.show
         fig = plt.gcf()
         fig.show() 
         fig.canvas.draw() 
-
-        while True:
-            # self._decay_heatmap(np_heatmap)
-            
-            np_heatmap = np.array(self.heatmap)
-            axis = plt.gca()
-            plt.imshow(np.transpose(np_heatmap), cmap='hot', interpolation='nearest')
-
-            fig = plt.gcf()
-            fig.show() 
-            fig.canvas.draw() 
-
-            # get input using method that calls listener node object 
-            # decay heatmap 
-            # update existing plot
-            # display plot 
-
-            time.sleep(TIME_TICK_LENGTH)
-
-if __name__ == "__main__":
-    # Acquire input source for realtime heatmap generation 
-    sensor_list_filepath = sys.argv[1]
-    config_filepath = sys.argv[2]
-    config = Config(config_filepath)
-
-    dynamic_heatmap = DynamicHeatmap(sensor_list_filepath, config) 
-    dynamic_heatmap.display_heatmap()
--- lurking_ai.py
+++ lurking_ai.py
@@ -1,27 +1,63 @@
 #!/usr/bin/env python
+"""
+    LurkingAI listens to sensor data and uses it to create a DynamicHeatmap 
+    object, and acquire a weighted average. 
+    Those two pieces of information are used to suggest an "optimal" location
+    for Ras to be in every TIME_TICK. 
+
+    Requirements:
+    - roscore must be running
+    - The publisher must be running in another terminal
+"""
+import sys
 import rospy
 
 from dynamic_heatmap import DynamicHeatmap
 from ras_msgs.msg import SensorPub
+from config import Config
 
-class ListenerNode():
+TIME_TICK = 1           # in seconds
+HEATMAP_DECAY_STRENGTH = 0.8   # how quickly the heatmap decays
+
+class LurkingAI():
     """
-        Subscribes to sensor data and publishes a location for Ras to go to. 
+        Subscribes to sensor data and publishes a landing location for Ras. 
     """
-    def __init__(self):
+    def __init__(self, dynamic_heatmap):
         print "nothing"
 
         self.listener()
 
     def callback(self, data):
-        DynamicHeatmap.update(data.name)
+        dynamic_heatmap.display_heatmap() 
         rospy.loginfo("%s was tripped" % (data.name))
 
+    """
+        Creates a listener node that acquires sensor data continuously. 
+    """
     def listener(self):
         rospy.init_node('sensor_listener', anonymous=True) 
         rospy.Subscriber("sensor_tripped", SensorPub, self.callback)
-        rospy.spin()
+        rospy.spin() # use spinonce?
+
+    """
+        Using a heatmap and weighted average, returns an "optimal"
+        landing zone for Ras at this current TIME_TICK. 
+    """
+    def get_landing_zone(self):
+        print (42,42)
 
 if __name__ == "__main__":
-    
-    listener = ListenerNode()
+    # Acquire filepaths 
+    sensor_list_filepath = sys.argv[1]
+    config_filepath = sys.argv[2]
+    config = Config(config_filepath)
+
+    # Create a dynamic heatmap object 
+    dynamic_heatmap = DynamicHeatmap(sensor_list_filepath, config) 
+
+    # Create a LurkingAI object that sends information to the heatmap 
+    lurking_ai = LurkingAI(dynamic_heatmap)
+
+    # dynamic_heatmap.display_heatmap()
+    # lurking_ai.get_landing_zone()
